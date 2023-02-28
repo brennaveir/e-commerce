ce92b0ce19ea09c2b245da05cac0bce6c80b4af4
brennaveir <brennaveir@hotmail.com>
2023-02-28 13:12:03 -0500

    added specifics to model associations in index.js

--- models/index.js
+++ models/index.js
@@ -6,22 +6,21 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-
-})
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
 Category.hasMany(Product, {
-
-})
+  foreignKey: 'product_id'
+});
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: '',
-  onDelete: 'CASCADE',
-})
+ through: ProductTag
+});
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: '',
-  onDelete: 'CASCADE',
-})
+  through: ProductTag
+});
 
 module.exports = {
   Product,
