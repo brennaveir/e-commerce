68d4c7881acba9de423e60b46023b0f999f567d9
brennaveir <brennaveir@hotmail.com>
2023-02-28 12:34:15 -0500

    added preliminary associations in models index.js

--- models/index.js
+++ models/index.js
@@ -5,12 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
 
+})
 // Categories have many Products
+Category.hasMany(Product, {
 
+})
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+})
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: '',
+  onDelete: 'CASCADE',
+})
 
 module.exports = {
   Product,
