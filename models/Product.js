7f1609ece3ea29e1baed469ac2c7b52cdb9042bc
brennaveir <brennaveir@hotmail.com>
2023-02-28 13:51:15 -0500

    added CRUD operations in category route

--- models/Product.js
+++ models/Product.js
@@ -4,44 +4,44 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // Initialize Product model (table) by extending off Sequelize's Model class
-class Product extends Model {}
+class Product extends Model { }
 
 // set up fields and rules for Product model
 Product.init(
   {
-   id: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    primaryKey: true,
-    autoIncrement: true,
-   },
-   product_name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-   },
-   price: {
-    type: DataTypes.DECIMAL,
-    allowNull: false,
-    //validate that the value is a decimal
-   },
-   stock: {
-    type: DataTypes.INTEGER,
-    allowNull: false,
-    //default value 10
-    defaultValue: 10,
-    //validates that the value is numeric
-    validate: {
-      isNumeric: true,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-   },
-   category_id: {
-    type: DataTypes.INTEGER,
-    //references the category model's id
-    references: {
-      model: 'category',
-      key: 'id'
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
-   },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      //validate that the value is a decimal
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      //default value 10
+      defaultValue: 10,
+      //validates that the value is numeric
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      //references the category model's id
+      references: {
+        model: 'category',
+        key: 'id'
+      },
+    },
   },
   {
     sequelize,
