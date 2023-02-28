ecac2df93987976e60280b721ee55dbfa9229fa4
brennaveir <brennaveir@hotmail.com>
2023-02-28 12:49:53 -0500

    added product database model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,39 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+   id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+   },
+   product_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+   },
+   price: {
+    type: DataTypes.DECIMAL,
+    allowNull: false,
+    //validate that the value is a decimal
+   },
+   stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    //default value 10
+    defaultValue: 10,
+    //validates that the value is numeric
+    validate: {
+      isNumeric: true,
+    },
+   },
+   category_id: {
+    type: DataTypes.INTEGER,
+    //references the category model's id
+    references: {
+      model: 'category',
+      key: 'id'
+    },
+   },
   },
   {
     sequelize,
